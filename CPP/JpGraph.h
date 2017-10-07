64ce2e5c43fae6b7cfe2365a45118cc1ead37ac7
MaloryVer9 <malory9@outlook.com>
2017-10-07 17:19:52 +0800

    upate
    重构了最短路径算法
    删除了并没有什么卵用的模板

--- CPP/JpGraph.h
+++ CPP/JpGraph.h
@@ -1,10 +1,12 @@
  //编写于2016.4.7
 //2016.4.9完成拓扑排序
+//2017.10.7对Dijkstra算法进行更新
 #pragma once
 #include<vector>
 #include<deque>
 #include<string>
 #include<iostream>
+#include<memory>
 #include <algorithm>
 #include <map>
 #include <stack>
@@ -15,7 +17,6 @@ namespace CjpSTL
 
 
 
-	template<typename T>
 	class JpGraph
 	{
 	public:
@@ -49,68 +50,8 @@ namespace CjpSTL
 		
 		JpGraph(){}
 
-		void lowestCost(const Vertex &v)
-		{
-			vector<pair<bool, int>>status;
-			for (int i = 0; i < vertexs.size();++i)
-			{
-				status.push_back(pair<bool,int>(false, INT32_MAX));
-			}
-			//建立与相应点集对应的集合。
-			status[v.index ].second = 0;
-			auto findMin = [&]()
-			{
-				int min = INT32_MAX;
-				int minIndx = 0;
-				for (int i = 0; i < status.size();++i)
-					if(status[i].second<min&&status[i].first==false)
-					{
-						min = status[i].second;
-						minIndx = i;
-					}
-				return minIndx;
-			};
-			auto isAllTrue = [&]()
-			{
-				
-				for(const auto & i : status)
-				{
-					if (i.first == false)
-						return false;
-				}
-				return true;
-			};
-			for (;;) {
-				status[findMin()].first = true; //每次都将未知的点集中的最小值设为已知
-				for (int i = 0; i < status.size(); ++i)
-				{
-					if (status[i].first == true)
-					{
-						for (const auto& adj : vertexs[i].degree)
-						{
-							auto desV = (*(adj.ptr)).index;
-							if (status[i].second + adj.cost < status[desV].second) //对与已知点相连接的点的cost进行更行
-								status[desV].second = status[i].second + adj.cost;
-
-
-						}
-
-					}
-
-
-				}
-				if (isAllTrue()) //如果所有点都已知，则退出循环
-					break;
-
-			}
-			for (int i = 0; i < status.size();++i)
-			{
-				cout << "vertexIndex :" << i << "   cost: " << status[i].second << endl;
-			}
-
-
-		}
-		static int getStaticVal();
+		void lowestCost(const Vertex& v);
+		
 		void buildGraph(vector<Vertex> vertexs);
 		void BFS(int index);
 		void DFS(int index);
@@ -119,22 +60,87 @@ namespace CjpSTL
 		vector<Vertex>vertexs;
 	};
 
-	template <typename T>
-	int JpGraph<T>::getStaticVal()
-	{
-		static int val = 4;
-		return val;
 
+	inline void JpGraph::lowestCost(const Vertex& v)
+	{
+		vector<pair<bool, int>> status;
+		for (int i = 0; i < vertexs.size(); ++i)
+		{
+			status.push_back(pair<bool, int>(false, INT32_MAX));
+		}
+		//建立与相应点集对应的集合。
+		status[v.index].second = 0;
+		auto findMin = [&]()
+		{
+			int min = INT32_MAX;
+			int minIndx = 0;
+			for (int i = 0; i < status.size(); ++i)
+				if (status[i].second < min && status[i].first == false)
+				{
+					min = status[i].second;
+					minIndx = i;
+				}
+			return minIndx;
+		};
+		auto isAllTrue = [&]()
+		{
+			for (const auto& i : status)
+			{
+				if (i.first == false)
+					return false;
+			}
+			return true;
+		};
+		//for (;;)
+		//{
+		//	status[findMin()].first = true; //每次都将未知的点集中的最小值设为已知
+		//	for (int i = 0; i < status.size(); ++i)
+		//	{
+		//		if (status[i].first == true)
+		//		{
+		//			for (const auto& adj : vertexs[i].degree)
+		//			{
+		//				auto desV = (*(adj.ptr)).index;
+		//				if (status[i].second + adj.cost < status[desV].second) //对与已知点相连接的点的cost进行更新
+		//					status[desV].second = status[i].second + adj.cost;
+		//			}
+		//		}
+		//	}
+		//	if (isAllTrue()) //如果所有点都已知，则退出循环
+		//		break;
+		//}
+		while(!isAllTrue())
+		{
+			auto minIndex = findMin();
+			status[minIndex].first = true; //每次都将未知的点集中的最小值设为已知
+		
+					for (const auto& adj : vertexs[minIndex].degree)
+					{
+						auto desV = (*(adj.ptr)).index;
+						if (status[minIndex].second + adj.cost < status[desV].second) //对与已知点相连接的点的cost进行更新
+							status[desV].second = status[minIndex].second + adj.cost;
+					}
+			
+		
+		}
+
+		for (int i = 0; i < status.size(); ++i)
+		{
+			cout << "vertexIndex :" << i << "   cost: " << status[i].second << endl;
+		}
 	}
 
-	template <typename T>
-	void JpGraph<T>::buildGraph(vector<Vertex> vertexs)
+
+
+
+
+	inline void JpGraph::buildGraph(vector<Vertex> vertexs)
 	{
 		this->vertexs = vertexs;
 	}
 
-	template <typename T>
-	void JpGraph<T>::BFS(int index)
+
+	inline void JpGraph::BFS(int index)
 	{
 		map<int, bool>hasVisited;
 		deque<int>checkLine;
@@ -169,8 +175,8 @@ namespace CjpSTL
 
 	}
 
-	template <typename T>
-	void JpGraph<T>::DFS(int index)
+
+	inline void JpGraph::DFS(int index)
 	{
 		map<int, bool>hasVisited;
 		for (int i = 0; i < vertexs.size(); ++i)
@@ -205,8 +211,8 @@ namespace CjpSTL
 		}
 	}
 
-	template <typename T>
-	void JpGraph<T>::topSort()
+
+	inline void JpGraph::topSort()
 	{
 		deque<Vertex>q;
 		for(const Vertex& vertex : vertexs)
@@ -234,25 +240,25 @@ namespace CjpSTL
 	inline void testTopSort()
 	{
 		vector<int>vec;
-		sort(vec.begin(), vec.end(), less<int>());
-		typedef JpGraph<string>::Vertex  vertex;
-		typedef JpGraph<string>::AdjAcent adj;
-		vertex v1(0, 0);
-		vertex v2(1, 1);
-		vertex v3(2, 1);
-		vertex v4(3, 2);
-		vertex v5(4, 2);
-		vertex v6(5, 3);
-		vertex v7(6, 2);
-		v1.degree = vector<adj>{ adj(1,&v4),adj{ 2,&v2 } };
-		v2.degree = vector<adj>{ adj{ 3,&v4 },adj{ 2,&v5 } };
-		v3.degree = vector<adj>{ adj{ 5,&v6 },adj{4,&v1} };
-		v4.degree = vector<adj>{ adj{ 8,&v6 },adj{ 2,&v3 },adj{2,&v5},adj{ 4,&v7 } };
-		v5.degree = vector<adj>{ adj{ 6,&v7 },};
-		v6.degree = vector<adj>();
-		v7.degree = vector<adj>{ adj{ 1,&v6 } };
-		JpGraph<string>graph;
-		graph.buildGraph(vector<vertex>{v1, v2, v3, v4, v5, v6, v7});
+		//sort(vec.begin(), vec.end(), less<int>());
+		typedef JpGraph::Vertex  vertex;
+		typedef JpGraph::AdjAcent adj;
+		vertex v0(0, 0);
+		vertex v1(1, 1);
+		vertex v2(2, 1);
+		vertex v3(3, 2);
+		vertex v4(4, 2);
+		vertex v5(5, 3);
+		vertex v6(6, 2);
+		v0.degree = vector<adj>{ adj(1,&v3),adj{ 2,&v1 } };
+		v1.degree = vector<adj>{ adj{ 3,&v3 },adj{ 2,&v4 } };
+		v2.degree = vector<adj>{ adj{ 5,&v5 },adj{4,&v0} };
+		v3.degree = vector<adj>{ adj{ 8,&v5 },adj{ 2,&v2 },adj{2,&v4},adj{ 4,&v6 } };
+		v4.degree = vector<adj>{ adj{ 6,&v6 },};
+		v5.degree = vector<adj>();
+		v6.degree = vector<adj>{ adj{ 1,&v5 } };
+		JpGraph graph;
+		graph.buildGraph(vector<vertex>{v0, v1, v2, v3, v4, v5, v6});
 		graph.topSort();
 	}
 
@@ -260,53 +266,54 @@ namespace CjpSTL
 	{
 		std::cout << "testDijkstra :" << std::endl;
 		vector<int>vec;
-		sort(vec.begin(), vec.end(), less<int>());
-		typedef JpGraph<string>::Vertex  vertex;
-		typedef JpGraph<string>::AdjAcent adj;
-		vertex v1(0, 0);
-		vertex v2(1, 1);
-		vertex v3(2, 2);
-		vertex v4(3, 3);
-		vertex v5(4, 1);
-		vertex v6(5, 3);
-		vertex v7(6, 2);
-		v1.degree = vector<adj>{ adj(1,&v4),adj{ 2,&v2 } };
-		v2.degree = vector<adj>{ adj{ 3,&v4 },adj{ 2,&v5 } };
-		v3.degree = vector<adj>{ adj{ 5,&v6 },adj{ 4,&v1 } };
-		v4.degree = vector<adj>{ adj{ 8,&v6 },adj{ 2,&v3 },adj{ 2,&v5 },adj{ 4,&v7 } };
-		v5.degree = vector<adj>{ adj{ 6,&v7 }, };
-		v6.degree = vector<adj>();
-		v7.degree = vector<adj>{ adj{ 1,&v6 } };
-		JpGraph<string>graph;
-		graph.buildGraph(vector<vertex>{v1, v2, v3, v4, v5, v6, v7});
-		graph.lowestCost(v1);
+		//sort(vec.begin(), vec.end(), less<int>());
+		typedef JpGraph::Vertex  vertex;
+		typedef JpGraph::AdjAcent adj;
+		vertex v0(0, 0);
+		vertex v1(1, 1);
+		vertex v2(2, 1);
+		vertex v3(3, 2);
+		vertex v4(4, 2);
+		vertex v5(5, 3);
+		vertex v6(6, 2);
+		v0.degree = vector<adj>{ adj(1,&v3),adj{ 2,&v1 } };
+		v1.degree = vector<adj>{ adj{ 3,&v3 },adj{ 2,&v4 } };
+		v2.degree = vector<adj>{ adj{ 5,&v5 },adj{ 4,&v0 } };
+		v3.degree = vector<adj>{ adj{ 8,&v5 },adj{ 2,&v2 },adj{ 2,&v4 },adj{ 4,&v6 } };
+		v4.degree = vector<adj>{ adj{ 6,&v6 }, };
+		v5.degree = vector<adj>();
+		v6.degree = vector<adj>{ adj{ 1,&v5 } };
+		JpGraph graph;
+		graph.buildGraph(vector<vertex>{v0, v1, v2, v3, v4, v5, v6});
+		graph.lowestCost(v0);
 
 
 	}
 
 	inline void testDFSAndBFS()
 	{
-		std::cout << "testDFS :" << std::endl;
+		
 		vector<int>vec;
-		sort(vec.begin(), vec.end(), less<int>());
-		typedef JpGraph<string>::Vertex  vertex;
-		typedef JpGraph<string>::AdjAcent adj;
-		vertex v1(0, 0);
-		vertex v2(1, 1);
-		vertex v3(2, 2);
-		vertex v4(3, 3);
-		vertex v5(4, 1);
-		vertex v6(5, 3);
-		vertex v7(6, 2);
-		v1.degree = vector<adj>{ adj(1,&v4),adj{ 2,&v2 } };
-		v2.degree = vector<adj>{ adj{ 3,&v4 },adj{ 2,&v5 } };
-		v3.degree = vector<adj>{ adj{ 5,&v6 },adj{ 4,&v1 } };
-		v4.degree = vector<adj>{ adj{ 8,&v6 },adj{ 2,&v3 },adj{ 2,&v5 },adj{ 4,&v7 } };
-		v5.degree = vector<adj>{ adj{ 6,&v7 }, };
-		v6.degree = vector<adj>();
-		v7.degree = vector<adj>{ adj{ 1,&v6 } };
-		JpGraph<string>graph;
-		graph.buildGraph(vector<vertex>{v1, v2, v3, v4, v5, v6, v7});
+		//sort(vec.begin(), vec.end(), less<int>());
+		typedef JpGraph:: Vertex  vertex;
+		typedef JpGraph:: AdjAcent adj;
+		vertex v0(0, 0);
+		vertex v1(1, 1);
+		vertex v2(2, 1);
+		vertex v3(3, 2);
+		vertex v4(4, 2);
+		vertex v5(5, 3);
+		vertex v6(6, 2);
+		v0.degree = vector<adj>{ adj(1,&v3),adj{ 2,&v1 } };
+		v1.degree = vector<adj>{ adj{ 3,&v3 },adj{ 2,&v4 } };
+		v2.degree = vector<adj>{ adj{ 5,&v5 },adj{ 4,&v0 } };
+		v3.degree = vector<adj>{ adj{ 8,&v5 },adj{ 2,&v2 },adj{ 2,&v4 },adj{ 4,&v6 } };
+		v4.degree = vector<adj>{ adj{ 6,&v6 }, };
+		v5.degree = vector<adj>();
+		v6.degree = vector<adj>{ adj{ 1,&v5 } };
+		JpGraph graph;
+		graph.buildGraph(vector<vertex>{v0, v1, v2, v3, v4, v5, v6});
+		std::cout << "testDFS :" << std::endl;
 		graph.DFS(0);
 		cout << "test BFS" << endl;
 		graph.BFS(0);
